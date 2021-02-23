3c6570b30368de25f4b2e51338f066ff823b8613
idris01 <idrys01@gmail.com>
2021-02-23 17:36:44 +0100

    near completion

---
+++ README.md
@@ -0,0 +1,6 @@
+#	Read the instructions to run the google.py module
+The module is run on commandline buy supplying a positional argument 
+e.g. python google.py <input_data_set>. The data set is as given from the 
+problem specification, an optional argument of the the output file name can 
+also be supplied with a flag -o as: python google.py <input_data_set> -o output_file_name.
+which is where the program will save its output inside the root directory .
--- google.py
+++ google.py
@@ -1,10 +1,18 @@
+import os
 import argparse
 from random import randint
+import datetime as d
+from pathlib import Path
+
+
 
 
 parser=argparse.ArgumentParser()
 parser.add_argument("file_name", help="name of file to open")
+parser.add_argument("--output_name","-o", help="name of output file")
 args=parser.parse_args()
+
+
 
 """ Takes in an input dataset and return a list 
 	[pizzas_count,T2,T3,T4] of integers
@@ -25,15 +33,26 @@ def get_pizzas_and_teams_count(file):
 
 
 
-def deliver_pizzas(file):
+def deliver_pizzas(file,output_file):
 	pizzas_and_teams=get_pizzas_and_teams_count(file)
 
 	teams=[{"t2":pizzas_and_teams[1]},{"t3":pizzas_and_teams[2]},{"t4":pizzas_and_teams[3]}]
 	
+	delivery_count=0;
+
+
+	delivery_file=""
 
 	ingredients=[]
 	count=0
-	print(f"Before Delivery {teams}")
+	
+
+	if output_file is not None and Path.is_dir(output_file) == False:
+		delivery_file=output_file
+	else:
+		delivery_file=f'output{d.datetime.now().__format__("%d%H%M%S")}.bin'
+	submit_file=open(delivery_file,"a")
+
 	with open(file,"r") as f:
 		while count <= pizzas_and_teams[0]+1:
 			data=f.readline()	# read one line from the imput dataset
@@ -49,16 +68,24 @@ def deliver_pizzas(file):
 				ingredients.sort(key=ingredients_count)
 				while(True):
 					s_team=choose_team(teams)
-					if choose_team==None or team_as_num(s_team) > len(ingredients):
+					if s_team==None or team_as_num(s_team) > len(ingredients):
+						ingredients.clear()
 						break
 
-					deliver(s_team,ingredients)
-				# ingredients.sort(key=ingredients_count)
-				# for i in ingredients:
-				# 	print(i)
+					
+					submit_file.write(deliver(s_team,ingredients))
+					delivery_count+=1
 				
-				print(f"After Delivery {teams}")
-				break
+				
+		print(f"{delivery_count} teams in total received pizzas")
+		print(f"{pizzas_and_teams}")
+		print(f"teams info after delivery {teams}")
+
+	submit_file.close()
+
+	#now to modify the outputfile to meeet specification
+
+	#with open(delivery_file,"r"):
 
 
 """since the ingredient is a list of dictionary, there is
@@ -75,7 +102,7 @@ def choose_team(teams):
 	""" Returns a string t2, t3 or t4 representing the randomly
 		selected team and return None if the there's no team
 	"""
-	# removes any empty team type
+	# removes any type of team whose number is zero
 	teams=list(filter(lambda x: x[list(x.keys())[0]] > 0,teams))
 		
 	if len(teams)==0:
@@ -106,18 +133,29 @@ def deliver(team,pizzas_available):
 	# the pizza index and the value being the list of ingredients in 
 	# in the pizza
 	p=pizzas_available
-
 	if(team=="t2"):
-		# delivering to a team of 2
-		print(2,list(p.pop().keys())[0],list(p.pop().keys())[0])
+		return (f"{2} {list(p.pop().keys())[0]} {list(p.pop().keys())[0]}\n")
 	elif(team=="t3"):
-		print(3,list(p.pop().keys())[0],list(p.pop().keys())[0],list(p.pop().keys())[0])
+		return (f"{3} {list(p.pop().keys())[0]} {list(p.pop().keys())[0]} {list(p.pop().keys())[0]}\n")
 	else:
-		print(4,list(p.pop().keys())[0],list(p.pop().keys())[0],list(p.pop().keys())[0],list(p.pop().keys())[0])
+		return (f"{4} {list(p.pop().keys())[0]} {list(p.pop().keys())[0]} {list(p.pop().keys())[0]} {list(p.pop().keys())[0]}\n")
+
+
+
+	# if(team=="t2"):
+	# 	# delivering to a team of 2
+	# 	print(2,list(p.pop().keys())[0],list(p.pop().keys())[0])
+	# elif(team=="t3"):
+	# 	print(3,list(p.pop().keys())[0],list(p.pop().keys())[0],list(p.pop().keys())[0])
+	# else:
+	# 	print(4,list(p.pop().keys())[0],list(p.pop().keys())[0],list(p.pop().keys())[0],list(p.pop().keys())[0])
 
 
 
 
 if __name__ == "__main__":
 	#print(help(args))
-	deliver_pizzas(args.file_name)
+	if args.output_name is None:
+		deliver_pizzas(args.file_name,None)
+	else:
+		deliver_pizzas(args.file_name,args.output_name)
